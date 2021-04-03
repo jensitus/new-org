8a15a630bffd9769cb0c91697fcb3f118395a4c4
jensitus <jens@service-b.org>
2021-04-03 21:40:39 +0200

    rename galleries

--- db/migrate/20210402121411_create_photo_galleries.rb
+++ db/migrate/20210402121411_create_photo_galleries.rb
@@ -1,5 +1,6 @@
 class CreatePhotoGalleries < ActiveRecord::Migration[6.1]
   def change
+    rename_table :photo_galleries, :galleries
     create_table :photo_galleries, id: :uuid do |t|
       t.references :photo, null: false, foreign_key: true, type: :uuid
       t.references :gallery, null: false, foreign_key: true, type: :uuid
