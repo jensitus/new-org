7aaac6f04b3a79f8913d217775785aef072b7358
jensitus <jens@service-b.org>
2021-04-06 20:44:28 +0200

    fuck the active storage

--- app/controllers/concerns/gallery_module.rb
+++ app/controllers/concerns/gallery_module.rb
@@ -4,6 +4,9 @@ module GalleryModule
     gallery_photos = []
     gallery.photos.each do |p|
       if p.image.attached?
+        puts '# # # # # # # # # # # # # # # # # # # # # # # # #'
+        puts p.image.variant(resize: "100x 100")
+        puts '# # # # # # # # # # # # # # # # # # # # # # # # #'
         gallery_photo = PhotoDto.new(p.id, rails_blob_url(p.image))
         gallery_photos.push(gallery_photo)
       end
