fd16a5eef2e540446e9cedbee8a16a28e726d0dd
jensitus <jens@service-b.org>
2021-05-06 21:57:11 +0200

    little bugfix

--- app/controllers/concerns/comment_module.rb
+++ app/controllers/concerns/comment_module.rb
@@ -3,7 +3,7 @@ module CommentModule
   def return_comment_list(comments)
     commentDtoList = Array.new
     comments.each do |c|
-      user = User.find_by(user_id: c.user_id)
+      user = User.find_by(id: c.user_id)
       commentDtoList << CommentDto.new(c.body, user.name, c.user_id, c.micropost_id, c.created_at, c.updated_at)
     end
     commentDtoList
